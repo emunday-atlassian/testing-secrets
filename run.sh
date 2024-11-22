a2392c3af72be50e1000e76cb432bd96e2b25d1d
emunday-atlassian <98496483+emunday-atlassian@users.noreply.github.com>
2024-11-22 17:20:13 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "MY_SECRET: ${MY_SECRET}"
-echo "MY_VARIABLE: ${MY_VARIABLE}"
+echo MY_SECRET: ${MY_SECRET}
+echo MY_VARIABLE: ${MY_VARIABLE}
 
-echo "MY_SECRET value: my\secret"
+echo MY_SECRET value: my\nsecret
 echo "MY_SECRET value: my
 secret"
